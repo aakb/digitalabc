164e97845ca39a5a953b7c2d1afca2ea770123e0
Troels Ugilt Jensen <tujensen@gmail.com>
2014-06-03 12:40:21 +0200

    Minor changes

--- backend/quiz.php
+++ backend/quiz.php
@@ -1,8 +1,22 @@
 <?php
-function getQuestions() {
-  $questions = file_get_contents("questions.json");
 
-  echo $questions;
+header('Access-Control-Allow-Origin: *');
+
+$action = "";
+if (isset($_POST["action"])) {
+  $action = $_POST["action"];
+} else if (isset($_GET["action"])) {
+  $action = $_GET["action"];
 }
 
-getQuestions();
+if ($action == "") {
+  return;
+}
+
+switch ($action) {
+  case "questions":
+    $questions = file_get_contents("questions.json");
+    echo $questions;
+    break;
+
+}
