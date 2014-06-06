131baa84a957baf8fd3c5b8bfd4ed4253e412acf
Troels Ugilt Jensen <tujensen@gmail.com>
2014-06-06 13:16:08 +0200

    Added some facebook integration, not done.

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -1,5 +1,9 @@
 abcApp.controller('IndexController', function($scope) {});
 
+abcApp.controller('ChallengeController', function($scope, $routeParams) {
+    $scope.resultID = $routeParams.result_id;
+})
+
 abcApp.controller('StartController', function($scope, $timeout) {
     $scope.text = "dine venner";
 
@@ -23,80 +27,80 @@ abcApp.controller('StartController', function($scope, $timeout) {
 });
 
 abcApp.controller('ShareController', function($scope, $location, quizFactory) {
-  if (!quizFactory.getQuizFinished()) {
-    $location.path('/');
-    return;
-  }
-  $scope.result = quizFactory.getResult();
-  $scope.id = "";
+    if (!quizFactory.getQuizFinished()) {
+        $location.path('/');
+        return;
+    }
+    $scope.result = quizFactory.getResult();
+    $scope.id = "";
 
-  quizFactory.saveResult().then(function(id) {
-      $scope.id = id;
-  });
+    quizFactory.saveResult().then(function(id) {
+        $scope.id = id;
+    });
 
-  $scope.submitResult = function() {
-      // Login: See https://developers.facebook.com/docs/facebook-login/login-flow-for-web/v2.0
+    $scope.submitResult = function() {
+        // Login: See https://developers.facebook.com/docs/facebook-login/login-flow-for-web/v2.0
 
-  }
+    }
 });
 
 abcApp.controller('QuizController', function($scope, $routeParams, $location, $timeout, quizFactory) {
-  quizFactory.init().then(function() {
-    if (quizFactory.getQuizFinished()) {
-      $location.path('/done');
-    }
-
-    $scope.step = $routeParams.step;
-    $scope.numberOfQuestions = quizFactory.getNumberOfQuestions();
-    $scope.highestQuestionAnswered = quizFactory.getHighestAnsweredQuestion();
-
-    if ($scope.step < 1 || $scope.step > $scope.highestQuestionAnswered) {
-      $location.path('/' + parseInt($scope.highestQuestionAnswered + 1));
-    }
-
-    $scope.question = quizFactory.getQuestion($scope.step);
-    $scope.chosen = quizFactory.getAnswer($scope.step);
-
-    $scope.nextStep = function() {
-      if ($scope.chosen.answer !== null) {
-        if ($scope.step < $scope.numberOfQuestions) {
-          $location.path('/' + (parseInt($scope.step) + 1));
+    quizFactory.init().then(function() {
+        if (quizFactory.getQuizFinished()) {
+            $location.path('/done');
         }
-        else if ($scope.step == $scope.numberOfQuestions) {
-          quizFactory.finishQuiz();
-          $('body').unbind("keydown keypress");
 
-          $location.path('/done');
+        $scope.step = $routeParams.step;
+        $scope.numberOfQuestions = quizFactory.getNumberOfQuestions();
+        $scope.highestQuestionAnswered = quizFactory.getHighestAnsweredQuestion();
+
+        if ($scope.step < 1 || $scope.step > $scope.highestQuestionAnswered) {
+            $location.path('/' + parseInt($scope.highestQuestionAnswered + 1));
         }
-      }
-    }
 
-    $scope.previousStep = function() {
-      if ($scope.step > 1) {
-        $location.path('/' + (parseInt($scope.step - 1)));
-      }
-    }
+        $scope.question = quizFactory.getQuestion($scope.step);
+        $scope.chosen = quizFactory.getAnswer($scope.step);
 
-    // jQuery to setup listeners for the keyboard.
-    $('body').unbind("keydown keypress");
-    $('body').bind("keydown keypress", function (event) {
-      if (event.which >= 49 && event.which <= 51 ) {
-          // Handles the 1,2,3 keys
-          $scope.chosen.answer = parseInt(event.which) - 49;
-          $scope.$apply();
-          $timeout($scope.nextStep, 500);
-          event.preventDefault();
-      }
-      else if (event.which == 37) {
-          // Handles the left arrow.
-          $timeout($scope.previousStep, 100);
-          event.preventDefault();
-      }
-      else if (event.which == 39) {
-          // Handles the right arrow.
-          $timeout($scope.nextStep, 100);
-          event.preventDefault();
-      }
+        $scope.nextStep = function() {
+            if ($scope.chosen.answer !== null) {
+                if ($scope.step < $scope.numberOfQuestions) {
+                    $location.path('/' + (parseInt($scope.step) + 1));
+                }
+                else if ($scope.step == $scope.numberOfQuestions) {
+                    quizFactory.finishQuiz();
+                    $('body').unbind("keydown keypress");
+
+                    $location.path('/done');
+                }
+            }
+        }
+
+        $scope.previousStep = function() {
+            if ($scope.step > 1) {
+                $location.path('/' + (parseInt($scope.step - 1)));
+            }
+        }
+
+        // jQuery to setup listeners for the keyboard.
+        $('body').unbind("keydown keypress");
+        $('body').bind("keydown keypress", function (event) {
+            if (event.which >= 49 && event.which <= 51 ) {
+                // Handles the 1,2,3 keys
+                $scope.chosen.answer = parseInt(event.which) - 49;
+                $scope.$apply();
+                $timeout($scope.nextStep, 500);
+                event.preventDefault();
+            }
+            else if (event.which == 37) {
+                // Handles the left arrow.
+                $timeout($scope.previousStep, 100);
+                event.preventDefault();
+            }
+            else if (event.which == 39) {
+                // Handles the right arrow.
+                $timeout($scope.nextStep, 100);
+                event.preventDefault();
+            }
+        });
     });
-  });
 });
--- web/js/routing.js
+++ web/js/routing.js
@@ -1,15 +1,15 @@
 abcApp.config(function($routeProvider) {$routeProvider
-  .when('/done', {
-    controller: 'ShareController',
-    templateUrl: '/partials/share.html'
-  })
-  .when('/:step', {
-    controller: 'QuizController',
-    templateUrl: '/partials/quiz.html'
-  })
-  .when('/', {
-    controller: 'StartController',
-    templateUrl: '/partials/quiz-start.html'
-  })
-  .otherwise({redirectTo: '/'});
+    .when('/done', {
+        controller: 'ShareController',
+        templateUrl: '/partials/quiz-finish.html'
+    })
+    .when('/:step', {
+        controller: 'QuizController',
+        templateUrl: '/partials/quiz.html'
+    })
+    .when('/', {
+        controller: 'StartController',
+        templateUrl: '/partials/quiz-start.html'
+    })
+    .otherwise({redirectTo: '/'});
 });
