52a7449e7d5f426c1deff42606e282a349c092a0
Thomas Johansen <thsj@aarhus.dk>
2014-06-18 13:39:44 +0200

    Added routing to controller(s)

--- src/DigitalABC/MainBundle/Controller/DefaultController.php
+++ src/DigitalABC/MainBundle/Controller/DefaultController.php
@@ -3,11 +3,20 @@
 namespace DigitalABC\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('DigitalABCMainBundle:Default:index.html.twig');
-    }
+
+/**
+ * @Route("/")
+ */
+class DefaultController extends Controller {
+
+  /**
+   * @Route("/")
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+  public function indexAction() {
+    return $this->render('DigitalABCMainBundle:Default:index.html.twig');
+  }
 }
