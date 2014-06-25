04a994dcce0795e6c7ff624bccdd4457d902afe1
Ture Gjørup <ture@ignatz.dk>
2014-06-25 16:00:21 +0200

    Refactored scroll to top

--- web/js/app.js
+++ web/js/app.js
@@ -1,4 +1,4 @@
 var abcApp = angular.module('abcApp', ['ngTouch', 'ngRoute', 'ngAnimate', 'duScroll'], function($interpolateProvider) {
     $interpolateProvider.startSymbol('<[');
     $interpolateProvider.endSymbol(']>');
-});
+}).value('duScrollDuration', 800);
