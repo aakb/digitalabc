d88f04c39572db3d05f0d29ba6a0d7ea52dbad5b
Thomas Johansen <thsj@aarhus.dk>
2014-07-24 11:00:22 +0200

    Issue 8005 by tj: Removed static json. Added dynamic questions

--- src/DigitalABC/MainBundle/Controller/ApiController.php
+++ src/DigitalABC/MainBundle/Controller/ApiController.php
@@ -66,4 +66,336 @@ class ApiController extends Controller {
 
     return new Response(json_encode($resultArray), 200);
   }
+
+  /**
+   * Get 10 questions.
+   *
+   * @Route("/questions")
+   *
+   * @return Response
+   */
+  public function getQuestionsAction() {
+    $questions = array(
+      array(
+        'question' => 'Hvad er Digital Selvbetjening?',
+        'answers' =>
+          array(
+            array(
+              'id' => 0,
+              'text' => 'Når de gamle på plejehjemmet har fjernbetjening til gardinerne',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'Når prisen på dine varer i supermarkedet automatisk registreres i kurven',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'Når man kan udfylde skemaer og få fat på kommunen på nettet',
+            ),
+          ),
+        'correctAnswer' => 2,
+      ),
+      array (
+        'question' => 'HVORDAN FÅR MAN UNGDOMS ELLER STUDIERABAT TIL BUSSEN?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'Spørger chaufføren - han er cool',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'Udfylder et skema, du får på din uddannelse og sender til Midttrafik',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'Du bruger bare dit NemID til at udfylde en ansøgning på nettet',
+            ),
+          ),
+        'correctAnswer' => 2,
+      ),
+      array (
+        'question' => 'HVAD ER NemBS?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'Et nyt realityshow hvor B.S. Christiansen dater PH-deltagere',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'En light version af borgerservice på biblioteket',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'Quickkassen i borgerservice på Rådhuset',
+            ),
+          ),
+        'correctAnswer' => 1,
+      ),
+      array (
+        'question' => 'HVAD ER ET NØGLEKORT?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'Et elektronisk kort, der erstatter gammeldags nøgler',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'En app, der finder dine nøgler via GPS',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'Et kort med koder, du bruger sammen med dit NemID',
+            ),
+          ),
+        'correctAnswer' => 2,
+      ),
+      array (
+        'question' => 'HVAD ER DIGITAL POSTKASSE?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'Indbakken på din e-mail',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'Det nye pakkepostsystem i supermarkederne',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'En personlig postkasse på nettet, hvor du får breve fra det offentlige',
+            ),
+          ),
+        'correctAnswer' => 2,
+      ),
+      array (
+        'question' => 'HVAD ER HOTLINE?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'Det direkte nummer til skadestuen',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'Et nummer, du kan ringe til, hvis du ikke fatter digital selvbetjening',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'En datingside for it-nørder',
+            ),
+          ),
+        'correctAnswer' => 1,
+      ),
+      array (
+        'question' => 'HVAD ER DET BLÅ SYGESIKRINGSBEVIS?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'Sygesikringskortet når du har vasket det sammen med dine nye jeans.',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'Sygesikringskort, som du skal bestille online og bruge i alle EU-lande',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'Sygesikringskort som skal bruges, hvis du rejser i Østen',
+            ),
+          ),
+        'correctAnswer' => 1,
+      ),
+      array (
+        'question' => 'HVAD ER FÆLLESOFFENTLIG KANALSTRATEGI?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'Plan for at der bliver smidt færre cykler i danske kanaler.',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'Plan for at begynde på digital kommunikation mellem borgere og det offentlige',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'DR’s plan for fordeling af licenspenge mellem radiokanalerne ',
+            ),
+          ),
+        'correctAnswer' => 1,
+      ),
+      array (
+        'question' => 'HVAD KAN DU KLARE MED DIGITAL SELVBETJENING?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'Stemme til folketingsvalg',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'Melde lægeskift',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'Bestille et pas',
+            ),
+          ),
+        'correctAnswer' => 2,
+      ),
+      array (
+        'question' => 'HVORDAN MODTAGER DU I FREMTIDEN BØDER FRA BIBLIOTEKET?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'De trækkes automatisk fra din fremtidige S.U.',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'I din digitale postkasse på nettet',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'Via Snapchat - så er de væk 10 sekunder senere',
+            ),
+          ),
+        'correctAnswer' => 1,
+      ),
+      array (
+        'question' => 'HVORDAN FÅR MAN SU PÅ SIN VIDEREGÅENDE UDDANNELSE?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'Taler med studievejlederen på din skole',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'Tager din mor og far med i banken og ansøger',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'Går på Borger.dk og bruger dit NemID til at ansøge',
+            ),
+          ),
+        'correctAnswer' => 2,
+      ),
+      array (
+        'question' => 'HVAD ER NemID?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'Et elektronisk ID-kort man kan vise til dørmænd',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'Elektronisk signatur på nettet som alle over 15 år skal bestille ',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'En app der identificerer berømtheder i nærheden',
+            ),
+          ),
+        'correctAnswer' => 1,
+      ),
+      array (
+        'question' => 'HVAD ER BORGER.DK',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'En netportal til køb og salg af borgere',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'En hjemmeside for borgmestre, der twerker som Miley',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'En portal for digital selvbetjening, hvor du kan tjekke post fra det offentlige',
+            ),
+          ),
+        'correctAnswer' => 2,
+      ),
+      array (
+        'question' => 'HVAD ER INKASSO?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'Mexicansk for det engelske udtryk ”in case of”',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'Processen der sørger for, at man betaler sin gæld til det offentlige',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'Vestjysk slang for pizzaæsker',
+            ),
+          ),
+        'correctAnswer' => 1,
+      ),
+      array (
+        'question' => 'HVAD SKER DER D. 1. NOVEMBER 2014?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'Det offentlige begynder at sende post til din digitale postkasse',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'Folketinget holder ”tag-dit-kæledyr-med-på-arbejde”-dag',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'Det offentlige begynder at tjekke, hvor meget tid du bruger på lektier',
+            ),
+          ),
+        'correctAnswer' => 0,
+      ),
+      array (
+        'question' => 'HVAD SKER DER D. 1. NOVEMBER 2014?',
+        'answers' =>
+          array (
+            array (
+              'id' => 0,
+              'text' => 'Det offentlige begynder at sende post til din digitale postkasse',
+            ),
+            array (
+              'id' => 1,
+              'text' => 'Folketinget holder ”tag-dit-kæledyr-med-på-arbejde”-dag',
+            ),
+            array (
+              'id' => 2,
+              'text' => 'Det offentlige begynder at tjekke, hvor meget tid du bruger på lektier',
+            ),
+          ),
+        'correctAnswer' => 0,
+      ),
+    );
+
+    $picks = array();
+    $items = array();
+    while (TRUE) {
+      if (sizeof($picks) == 10) {
+        break;
+      }
+      $item = rand(0, 9);
+
+      if (!in_array($item, $picks)) {
+        $picks[] = $item;
+        $items[] = $questions[$item];
+      }
+    }
+
+    return new Response(json_encode($items));
+  }
 }
